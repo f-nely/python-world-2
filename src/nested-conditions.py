7fae19e095ef343a801a9ced64444b3f31dc657e
nely de faria <nelly.nf54@gmail.com>
2025-12-12 12:19:07 -0300

    Create nested-conditions.py

---
+++ src/nested-conditions.py
@@ -0,0 +1,9 @@
+# Example one of if-elif-else condition
+
+z = 3
+if z % 2 == 0:
+  print("z is divisible by 2")
+elif z % 3 == 0:
+  print("z is divisible by 3")
+else:
+  print("z is neither divisible by 2 or by 3")
