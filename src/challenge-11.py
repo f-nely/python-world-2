9a81be8593eb3efeb6cdd35ee07b9afc26059060
nely de faria <nelly.nf54@gmail.com>
2025-12-20 11:35:20 -0300

    Create challenge-11.py

---
+++ src/challenge-11.py
@@ -0,0 +1,7 @@
+"""Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles"""
+
+from time import sleep
+for i in range(10, -1, -1):
+  print(i)
+  sleep(0.5)
+print("🎆")
